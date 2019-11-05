9dd87127d7dcfb50cf91b6a856d0b095557a275d
“stevenkei” <slota200@caledonian.ac.uk>
2019-11-05 19:46:42 +0000

    edit 4

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 
 class Dec2Hex
@@ -51,44 +50,3 @@ class Dec2Hex
 
     }
 }
-=======
-import java.util.Scanner;
-
-class Dec2Hex
-{
-
-    public static int Arg1; //steven
-
-    public static void main(String args[])
-    {
-        if (args.length > 0) 
-        {
-            try 
-            {
-                Arg1 = Integer.parseInt(args[0]);
-            } 
-            catch (NumberFormatException e) 
-            {
-                System.err.println("Argument" + args[0] + " must be an integer.");
-                System.exit(1);
-            }
-        }
-
-    	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-         int rem, num;
-	num = Arg1;
-	String hexadecimal=""; 
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-        
-        while(num != 0)
-        {
-	    rem=num%16;
-	    hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
-        }
-        
-        System.out.println("Hexadecimal representation is : " + hexadecimal);
-
-    }
-}
->>>>>>> 445f02f13a10b5e9252801866b583a4a5abf520a
